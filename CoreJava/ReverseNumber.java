71408051ff0c9849c0dabecd3011260fd675bfd3
krishnapriya <krithikakrishnapriya@gmail.com>
2026-02-07 23:21:45 -0800

    Update

---
+++ CoreJava/ReverseNumber.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class ReverseNumber {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+        int reversed = 0;
+
+        while(num!=0){
+            int digit = num%10;
+            reversed = (reversed *10) + digit;
+            num = num/10; 
+        }
+
+        System.out.println("Reversed number: " + reversed);
+
+        if(reversed==num){
+            System.out.println("The number is palindrome");
+        }
+        else{
+            System.out.println("The number is not a palindrome");
+        }
+
+        sc.close();
+    }
+}
+
--- CorePython/RegularExpresions.py
+++ CorePython/RegularExpresions.py
@@ -16,4 +16,31 @@ text =" The company phone number is : 1234567890"
 match = re.search(pattern,text)
 print(match.group())
 
+#Using find all instaces of a pattern method
 
+pattern = r"[aeiouAEIOU]"
+text = "Python Programming Language"
+match = re.findall(pattern, text)
+print(match)
+
+
+# Real World applications
+
+# email verification
+pattern =r"^[\w\.-]+@[\w\.-]+\.\w+$"
+text = input("enter your email address: ")
+match = bool(re.match(pattern,text))
+if (match):
+    print("valid email address")
+else:
+    print("Invalid Email address") 
+
+
+# Password strength check
+pattern = r"^(?=.*[a-z])(?=.*[A-Z])(?=.*0-9)"
+password = input("Enter your python: ")
+match = bool(re.match(pattern, password))
+if match:
+    print("Your password is strong")
+else:
+    print("Your password is weak")       
