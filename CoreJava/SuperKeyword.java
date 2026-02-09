02648e8b55ed12a015acb2018122b6e2abcc0e52
krishnapriya Shankar <54299282+krishnapriya-21@users.noreply.github.com>
2026-02-09 03:55:25 -0800

    SuperKeywordUpdate

--- CoreJava/SuperKeyword.java
+++ CoreJava/SuperKeyword.java
@@ -11,6 +11,10 @@
 class Animal{
     String name = "Animal";
     
+    Animal(){
+        System.out.println("Animal Constructor called");
+    }
+
     void sound(){
         System.out.println("Animal makes a sound");
     }
@@ -21,10 +25,13 @@ class Dog extends Animal{
    
     String name = "Dog";
 
+    static int count =0;
+
     //Calling Animal constructor
 
     Dog(){
         super();  // calls the constructor of the parent class (Animal)
+        count++; // Incrementing the count og dog instances
     }
 
     void sound(){
@@ -36,6 +43,10 @@ class Dog extends Animal{
         System.out.println(" Parent class name: " + super.name);
         System.out.println("Child class name:" + this.name);
     }
+
+    static void displayCount(){
+        System.out.println("Number of dog instances :" + count);
+    }
 }
 
 
@@ -45,5 +56,12 @@ public class SuperKeyword {
         Dog dog = new Dog();
         dog.sound();
         dog.displayNames();
+
+
+        //Purpose of count (static variable):  
+        //In the example, count is a static variable that tracks how many Dog objects have been created.
+        //Every time you call new Dog(), the constructor increments count.
+        Dog dog2 = new Dog();
+        Dog.displayCount();
     }
 }
