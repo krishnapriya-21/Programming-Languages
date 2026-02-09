5a22ccdc5244471330863c551dd4065bf5b5b8c1
krishnapriya Shankar <54299282+krishnapriya-21@users.noreply.github.com>
2026-02-09 04:20:20 -0800

    String class upload

---
+++ CoreJava/StringClass.java
@@ -0,0 +1,67 @@
+//1. What is a String?
+//A String in Java is an object that represents a sequence of characters.
+
+//Unlike primitive types (int, char, etc.), Strings are immutable objects—once created, their value cannot be changed.
+
+//Strings are widely used for text manipulation in Java programs.
+
+//Key Properties
+//Immutable: Any modification (like concatenation) creates a new String object.
+
+//Stored in String Pool: Java optimizes memory by reusing string literals.
+
+//Belongs to java.lang package: No need to import explicitly.
+
+//Mutable Alternatives
+//StringBuilder and StringBuffer are mutable versions.
+
+//Use them when frequent modifications are needed (e.g., loops, concatenations).
+
+
+
+public class StringClass {
+
+    public static void main(String[] args){
+
+        // creating a string in string literal
+
+        String str = "Java Programming";
+
+        //Basic methods
+
+        System.out.println("Length od string:" + str.length());
+        System.out.println("Character at index 5:" + str.charAt(5));
+        System.out.println("Substring from index 5 to 11:" + str.substring(5,11));
+        System.out.println("String in uppercase:" + str.toUpperCase());
+        System.out.println("String trimmed:" + str.trim());
+        System.out.println("String replaced:" + str.replace("Java", "Python"));
+
+        //Searching in string
+        System.out.println("Index of 'Programming':" + str.indexOf("Programming"));
+        System.out.println("Does string contain java? " + str.contains("Java"));
+
+
+        //splitting string
+        String[] words = str.trim().split(" ");
+        System.out.println(" Words in the string:");
+        for(String word: words){
+            System.out.println(word);
+        }
+
+        //comparison of strings
+        string str2 = new String("Java Programming");
+
+        System.out.println("== comparison:" + (str == str2));
+        System.out.println("equals () comparison:" + str.equals(str2));
+
+
+        //String Builder 
+
+        StringBuilder sb = new StringBuilder("Hello");
+        sb.append("World !");
+        System.out.println(sb);
+
+
+    }
+    
+}
